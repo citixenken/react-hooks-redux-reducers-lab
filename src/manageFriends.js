02e5491a237911d335035667390e783a76da2dba
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-10-08 09:34:06 +0300

    commit tree ed6e767bd6c65cf072e4b0797c5073b45baafaf1

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,17 @@
 export function manageFriends(state, action) {
-  // your code here
+  switch (action.type) {
+    case "friends/add":
+      return { friends: [...state.friends, action.payload] };
+    case "friends/remove":
+      return {
+        friends: state.friends.filter((friend) => friend.id !== action.payload),
+      };
+    default:
+      return state;
+  }
 }
+// let state = { friends: [] };
+// let action_a = { type: "friends/add" };
+// let action_b = { type: "friends/remove" };
+// manageFriends(state, action_a);
+// manageFriends(state, action_b);
